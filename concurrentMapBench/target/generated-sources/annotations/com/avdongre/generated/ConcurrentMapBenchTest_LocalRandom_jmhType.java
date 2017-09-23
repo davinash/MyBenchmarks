5281225a513b5032b82ee02bce710e6a143b9c1b
Avinash <avinash@ampool.io>
2017-09-23 15:24:48 +0530

    Initial Geode Get Perf using JMH

---
+++ concurrentMapBench/target/generated-sources/annotations/com/avdongre/generated/ConcurrentMapBenchTest_LocalRandom_jmhType.java
@@ -0,0 +1,4 @@
+package com.avdongre.generated;
+public class ConcurrentMapBenchTest_LocalRandom_jmhType extends ConcurrentMapBenchTest_LocalRandom_jmhType_B3 {
+}
+
---
+++ concurrentMapBench/target/generated-sources/annotations/com/avdongre/generated/ConcurrentMapBenchTest_jmhType.java
@@ -0,0 +1,4 @@
+package com.avdongre.generated;
+public class ConcurrentMapBenchTest_jmhType extends ConcurrentMapBenchTest_jmhType_B3 {
+}
+
